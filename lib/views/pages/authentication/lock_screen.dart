b82fe3b6b31c81cd2b9e82a559829d861280a02e
kedwithGod <Komolafeezekiel123@gmail.com>
2025-08-26 21:12:10 +0100

    started lock screen

---
+++ lib/views/pages/authentication/lock_screen.dart
@@ -0,0 +1,12 @@
+
+
+import '../../../model/utilities/imports/generalImport.dart';
+
+class LockScreenPage extends StatelessWidget {
+  const LockScreenPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
